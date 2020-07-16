76259a1fb7540f093ac976ae1fff30d20ed43b50
Tom Steele <tsteele@madisoncollege.edu>
2020-07-16 14:46:09 -0500

    Parsing error and fragments...
    Also show that missing closing tags are now compiler errors...
    
    JSX is JS ! ! !

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const MyComponent = () => {
     return (
-        <h1>
-            My Component ! ! !
-        </h1>
+        <>
+            <h1>
+                My Component ! ! !
+            </h1>
+            <ul>
+                <li>Tom</li>
+                <li>Taylor</li>
+            </ul>
+        </>
     )
 }
 
